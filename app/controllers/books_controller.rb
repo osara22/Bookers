3b143b5a204c59dc934ffbd7d1678322af4b3e24
osara22 <osaraiya22@gmail.com>
2021-07-07 10:46:37 +0000

    [Finish]bookers_1

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,57 @@
+class BooksController < ApplicationController
+  def index
+    @books = Book.all
+    @book = Book.new
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @books = Book.all
+    # 1. データを新規登録するためのインスタンス作成
+    @book = Book.new(book_params)
+    # 2. データをデータベースに保存するためのsaveメゾッド実行&サクセスメッセージの表示
+    if @book.save
+      flash[:notice] = "Book was successfully created."
+      # 詳細画面へリダイレクト
+      redirect_to book_path(@book.id)
+    else
+      render :index
+    end
+    
+  end
+
+  def edit
+    @book = Book.find(params[:id])
+  end
+
+  def update
+    @book = Book.find(params[:id])
+    # binding.pry
+    if @book.update(book_params)
+      flash[:notice] = "Book was successfully updated."
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
+    
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
+    # params.permit(:title, :body)
+  end
+end
